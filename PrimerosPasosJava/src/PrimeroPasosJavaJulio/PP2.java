320ac45b2b6ed881acbc6b329f02d74895c7db66
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-07-12 20:23:56 -0600

    pp2 Commit

---
+++ PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP2.java
@@ -0,0 +1,9 @@
+package PrimeroPasosJavaJulio;
+
+public class PP2 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase PP2.. primeros pasos en java con eclipse y GIT ");
+	}
+
+}
