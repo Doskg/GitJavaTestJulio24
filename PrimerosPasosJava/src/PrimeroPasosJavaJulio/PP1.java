4f633094527f445fd1ce9eab7d63e09393c50674
Oscar Gonzalez <oscar.gonzalez.baldizon@gmail.com>
2024-07-12 20:56:48 -0600

    pp1

--- PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP1.java
+++ PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP1.java
@@ -4,6 +4,8 @@ public class PP1 {
 
 	public static void main(String[] args) {
 		System.out.println("Primeros pasos en Java curso Youtube Pildoras Informaticas");
+		System.out.println("Este es un programa en Java pp1");
+		
 	}
 
 }
