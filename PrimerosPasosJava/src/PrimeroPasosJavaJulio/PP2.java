5b5d248836d6ed412ebd7624054ee1119074f7bd
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-07-12 21:19:58 -0600

    joptionPane1

--- PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP2.java
+++ PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP2.java
@@ -1,14 +1,18 @@
 package PrimeroPasosJavaJulio;
 
+import javax.swing.JOptionPane;
+
 public class PP2 {
 
 	public static void main(String[] args) {
+		String Cadena;
 		System.out.println("Clase PP2.. primeros pasos en java con eclipse y GIT ");
 
 
 		System.out.println("Este es un ejemplo para Git..!");
 
-		JOptionPane.showMessageDialog(null, "Esta una classe");
+		Cadena= JOptionPane.showInputDialog(null, "Esta una classe");
+		JOptionPane.showMessageDialog(null,Cadena);
 
 
 
