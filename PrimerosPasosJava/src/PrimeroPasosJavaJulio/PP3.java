641533a158723208ae144d38bb8594c216a74134
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-07-12 21:51:38 -0600

    math refundicion

--- PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP3.java
+++ PrimerosPasosJava/src/PrimeroPasosJavaJulio/PP3.java
@@ -5,17 +5,21 @@ public class PP3 {
 	public static void main(String[] args) {
 		//Calculos numericos. Video #9
 		
-		double raiz= Math.sqrt(3.0);
-		double raiz2= Math.pow(2,3);
-		double seno= Math.sin(raiz2);
+//		double raiz= Math.sqrt(3.0);
+//		double raiz2= Math.pow(2,3);
+//		double seno= Math.sin(raiz2);
+//		
+//		System.out.println("Raiz: "+raiz);
+//		System.out.println("Potencia: "+raiz2);
+//		System.out.println("Seno: "+ seno);
 		
-		System.out.println("Raiz: "+raiz);
-		System.out.println("Potencia: "+raiz2);
-		System.out.println("Seno: "+ seno);
 		
+		double num1=15.65;
 		
+		int  res =(int)Math.round(num1);
+		System.out.println(res);
 		
-		 
+
 
 	}
 
